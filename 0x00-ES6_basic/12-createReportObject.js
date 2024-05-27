260cfe4ebe90c799beb1aed38ce8a00e4f51d908
Dridi Chaith <dridi.chaith@gmail.com>
2024-05-27 20:38:07 +0100

    task 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+export default function createReportObject(employeesList) {
+  const report = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return report;
+}
