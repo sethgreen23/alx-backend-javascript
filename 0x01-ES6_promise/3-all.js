751f136ecf7f182ac5f3547daddd3c510b0b6bbe
Dridi Chaith <dridi.chaith@gmail.com>
2024-05-28 22:46:37 +0100

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default async function handleProfileSignup() {
   try {
     const [data1, data2] = await Promise.all([uploadPhoto(), createUser()]);
-    console.log(`${data1.body} ${data2.firstName} ${data2.lastName}`)
+    console.log(`${data1.body} ${data2.firstName} ${data2.lastName}`);
   } catch (error) {
     console.log('Signup system offline');
   }
