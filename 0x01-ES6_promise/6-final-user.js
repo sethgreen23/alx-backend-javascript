82a011ec7b91bbd043b1b701e48b5ccf1f11724c
Dridi Chaith <dridi.chaith@gmail.com>
2024-05-29 00:37:33 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const results = await Promise.allSettled(
-    [signUpUser(firstName, lastName), uploadPhoto(fileName)],
-  );
-  return [
-    {
-      status: results[0].status,
-      value: results[0].value,
-    },
-    {
-      status: results[1].status,
-      value: results[1].reason,
-    },
-  ];
+  const user = await signUpUser(firstName, lastName).then((value) => ({
+    status: 'resoved',
+    value,
+  }));
+  const photo = await uploadPhoto(fileName).catch((value) => ({
+    status: 'rejected',
+    value: value.message,
+  }));
+  return [user, photo];
 }
