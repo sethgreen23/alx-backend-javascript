a6e3f40761271154d92926399930d153c65e8c7d
Dridi Chaith <dridi.chaith@gmail.com>
2024-05-29 01:24:26 +0100

    task 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,10 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw Error('cannot divide by 0');
-    }
-  } catch (e) {
-    return e;
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
   }
   return numerator / denominator;
 }
