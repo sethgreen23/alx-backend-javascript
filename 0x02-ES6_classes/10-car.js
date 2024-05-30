b8563797b8c15233851b9513615a8aee0301dd5f
Dridi Chaith <dridi.chaith@gmail.com>
2024-05-30 20:58:51 +0100

    task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,16 +1,20 @@
+const _brand = Symbol('_brand');
+const _motor = Symbol('_motor');
+const _color = Symbol('_color');
+
 export default class Car {
   constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
   }
 
   cloneCar() {
     const current = this;
     return Object.assign(Object.create(Object.getPrototypeOf(current)), {
-      _brand: undefined,
-      _motor: undefined,
-      _color: undefined,
+      [_brand]: undefined,
+      [_motor]: undefined,
+      [_color]: undefined,
     });
   }
 }
