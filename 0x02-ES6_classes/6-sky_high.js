f1d375d3986f95fee6d55ddc45c54fdd1022057b
Dridi Chaith <dridi.chaith@gmail.com>
2024-05-30 17:43:00 +0100

    task 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,17 +3,21 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (floors.constructor !== Number) {
-      throw new TypeError('Floors must be a Number');
-    }
-    this._floors = floors;
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
+  set floors(floors) {
+    if (floors.constructor !== Number) {
+      throw new TypeError('Floors must be a Number');
+    }
+    this._floors = floors;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly ${this._floors} floors`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
