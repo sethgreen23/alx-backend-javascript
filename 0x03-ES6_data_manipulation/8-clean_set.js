ec00f6f3b01876c208dcaf900ac6aaaea9c2baef
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-04 00:54:13 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (typeof set !== 'object') return '';
+  if (!(set instanceof Set)) return '';
   if (typeof startString !== 'string') return '';
   if (startString.length === 0) return '';
   return [...set].filter((value) => value.slice(0, startString.length) === startString).map((value) => value.slice(startString.length)).join('-');
