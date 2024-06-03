c48cc1de52b9a4d5eefb62e23ea1e05adf88beab
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-03 19:45:53 +0100

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,12 +2,12 @@ export default function updateStudentGradeByCity(students, location, newGrades) 
   return students.filter((obj) => obj.location === location).map((ob) => {
     for (const gradeObj of newGrades) {
       if (gradeObj.studentId === ob.id) {
-        const objClone = JSON.parse(JSON.stringify(ob));
+        const objClone = { ...ob };
         objClone.grade = gradeObj.grade;
         return objClone;
       }
     }
-    const objClone = JSON.parse(JSON.stringify(ob));
+    const objClone = { ...ob };
     objClone.grade = 'N/A';
     return objClone;
   });
