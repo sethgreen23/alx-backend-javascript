42f2243308b8687fbf9b2470aac0be7bcd108fe1
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-03 19:07:48 +0100

    task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
-export default getStudentsByLocation() {
-	
+export default function getStudentsByLocation(students, location) {
+  if (Array.isArray(students) && typeof location === 'string') {
+    return students.filter((obj) => obj.location === location);
+  }
+  return [];
 }
