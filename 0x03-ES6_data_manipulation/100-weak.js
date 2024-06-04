1166d13b7b11a8cc26aa85871c4237e8eab96a36
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-04 02:06:33 +0100

    task 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -6,5 +6,4 @@ export function queryAPI(endpoint) {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   }
   if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
-  return weakMap.get(endpoint);
 }
