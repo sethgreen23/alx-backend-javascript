6c401a49a01e18240c948b738ffa3029ba0f236f
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-22 17:01:47 +0100

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  if (input != null) process.stdout.write(`Your name is: ${input}`);
+  if (input != null) process.stdout.write(`Your name is: ${input.toString()}`);
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing.\n');
