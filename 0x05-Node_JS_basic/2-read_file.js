b4020f9057d3fb1552091a1bf2d60bfe41424208
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-22 16:08:32 +0100

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,33 +5,26 @@ const sweList = [];
 const csList = [];
 const countStudents = (path) => {
   try {
-    fs.createReadStream(path, { encoding: 'utf-8' })
-      .on('data', (chunk) => {
-        if (chunk !== '') generalList = chunk.split('\n');
-      })
-      .on('end', () => {
-        if (generalList.length > 1) {
-          generalList.forEach((line, index) => {
-            if (index !== 0 && line !== '') {
-              const lineList = line.split(',');
-              if (lineList[3] === 'CS') {
-                csList.push(lineList[0]);
-              } else {
-                sweList.push(lineList[0]);
-              }
-            }
-          });
-          const totalStudents = csList.length + sweList.length;
-          console.log(`Number of students: ${totalStudents}`);
-          if (csList.length > 0) console.log(`Number of students in CS: ${csList.length}. List: ${csList.join(', ')}`);
-          if (sweList.length > 0) console.log(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
+    const content = fs.readFileSync(path, { encoding: 'utf-8' });
+    if (content !== '') generalList = content.split('\n');
+    if (generalList.length > 1) {
+      generalList.forEach((line, index) => {
+        if (index !== 0 && line !== '') {
+          const lineList = line.split(',');
+          if (lineList[3] === 'CS') {
+            csList.push(lineList[0]);
+          } else {
+            sweList.push(lineList[0]);
+          }
         }
-      })
-      .on('error', () => {
-        throw new Error('Cannot load the database');
       });
+      const totalStudents = csList.length + sweList.length;
+      console.log(`Number of students: ${totalStudents}`);
+      if (csList.length > 0) console.log(`Number of students in CS: ${csList.length}. List: ${csList.join(', ')}`);
+      if (sweList.length > 0) console.log(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
+    }
   } catch (error) {
-    console.log(error);
+    throw new Error('Cannot load the database');
   }
 };
 
