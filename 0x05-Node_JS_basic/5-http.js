e6e71ccbb9f27266fc786608ea78afea89662ffd
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-23 20:12:28 +0100

    task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,55 +1,67 @@
 const http = require('http');
 const fs = require('fs');
 
-const countStudents = (path) => new Promise((resolve, reject) => {
-  let generalList = [];
-  const sweList = [];
-  const csList = [];
-  fs.createReadStream(path, { encoding: 'utf-8' })
-    .on('data', (chunk) => {
-      if (chunk !== '') generalList = chunk.split('\n');
-    })
-    .on('end', () => {
-      if (generalList.length > 1) {
-        let response = '';
-        generalList.forEach((line, index) => {
-          if (index !== 0 && line.trim() !== '') {
-            const lineList = line.trim().split(',');
-            if (lineList[3] === 'CS') {
-              csList.push(lineList[0]);
-            } else if (lineList[3] === 'SWE') {
-              sweList.push(lineList[0]);
-            }
-          }
-        });
-        const totalStudents = csList.length + sweList.length;
-        response += `Number of students: ${totalStudents}\n`;
-        if (csList.length > 0) response += `Number of students in CS: ${csList.length}. List: ${csList.join(', ')}\n`;
-        if (sweList.length > 0) response += `Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`;
-        return resolve(response);
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (err) return reject(Error('Cannot load the database'));
+      // split data and taking only list without header
+      const lines = data.split('\n').slice(1, -1);
+      // give the header of data
+      const header = data.split('\n').slice(0, 1)[0].split(',');
+      // find firstname and field index
+      const idxFn = header.findIndex((ele) => ele === 'firstname');
+      const idxFd = header.findIndex((ele) => ele === 'field');
+      // declarate two dictionaries for count each fields and store list of students
+      const fields = {};
+      const students = {};
+      // it will contain all data
+      const all = {};
+
+      lines.forEach((line) => {
+        const list = line.split(',');
+        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
+        fields[list[idxFd]] += 1;
+        if (!students[list[idxFd]]) students[list[idxFd]] = '';
+        students[list[idxFd]] += students[list[idxFd]]
+          ? `, ${list[idxFn]}`
+          : list[idxFn];
+      });
+
+      all.numberStudents = `Number of students: ${lines.length}\n`;
+      all.listStudents = [];
+      for (const key in fields) {
+        if (Object.hasOwnProperty.call(fields, key)) {
+          const element = fields[key];
+          all.listStudents.push(`Number of students in ${key}: ${element}. List: ${students[key]}`);
+        }
       }
-      return reject(new Error('Cannot load the database'));
-    })
-    .on('error', () => reject(new Error('Cannot load the database')));
-});
+      return resolve(all);
+    });
+  });
+}
+
+const hostname = '127.0.0.1';
+const port = 1245;
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  const endpoint = req.url;
-  if (endpoint === '/students') {
-    countStudents(process.argv[2]).then((data) => {
-      res.end(`This is the list of our students\n${data}`);
-    })
-      .catch((error) => {
-        res.statusCode = 500;
-        res.end(error.message);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') res.end('Hello Holberton School!');
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.write(data.numberStudents);
+        res.write(data.listStudents.join('\n'));
+        res.end();
+      })
+      .catch((err) => {
+        res.end(err.message);
       });
-  } else if (endpoint === '/') {
-    res.end('Hello Holberton School!');
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
   }
-}).listen(1245);
+});
+
+app.listen(port, hostname);
 
 module.exports = app;
