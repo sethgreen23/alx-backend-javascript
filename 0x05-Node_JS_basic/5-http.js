15c1515558efe98743fb954aeddfdf2b3d66e763
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-23 01:48:19 +0100

    task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,36 +1,35 @@
 const http = require('http');
 const fs = require('fs');
 
-const countStudents = (path = 'database.csv') => new Promise((resolve, reject) => {
+const countStudents = (path = './database.csv') => new Promise((resolve, reject) => {
   let generalList = [];
   const sweList = [];
   const csList = [];
-  fs.createReadStream(path, { encoding: 'utf-8' })
-    .on('data', (chunk) => {
-      if (chunk !== '') generalList = chunk.split('\n');
-    })
-    .on('end', () => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    } else {
+      generalList = data.split('\n').filter((line) => line.trim() !== '');
       if (generalList.length > 1) {
         let response = '';
-        generalList.forEach((line, index) => {
-          if (index !== 0 && line.trim() !== '') {
-            const lineList = line.trim().split(',');
-            if (lineList[3] === 'CS') {
-              csList.push(lineList[0]);
-            } else if (lineList[3] === 'SWE') {
-              sweList.push(lineList[0]);
-            }
+        generalList.slice(1).forEach((line) => {
+          const lineList = line.split(',');
+          if (lineList[3] === 'CS') {
+            csList.push(lineList[0]);
+          } else if (lineList[3] === 'SWE') {
+            sweList.push(lineList[0]);
           }
         });
         const totalStudents = csList.length + sweList.length;
         response += `Number of students: ${totalStudents}\n`;
         if (csList.length > 0) response += `Number of students in CS: ${csList.length}. List: ${csList.join(', ')}\n`;
         if (sweList.length > 0) response += `Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`;
-        return resolve(response);
+        resolve(response);
+      } else {
+        reject(new Error('Cannot load the database'));
       }
-      return reject(new Error('Cannot load the database'));
-    })
-    .on('error', () => reject(new Error('Cannot load the database')));
+    }
+  });
 });
 
 const path = process.argv[2];
