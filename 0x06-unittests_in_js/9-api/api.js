2884d212080438b1beaf31a8686a8f5981683ae1
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-26 01:27:23 +0100

    task 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -8,6 +8,10 @@ app.get('/cart/:id(\\d+)', (req, res) => {
 	res.send(`Payment methods for cart ${req.params.id}`);
 })
 
+app.get('/', (req, res) => {
+	res.send('Welcome to the payment system');
+})
+
 app.listen(port, () => {
 	console.log('API available on localhost port 7865');
 })
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -13,6 +13,15 @@ describe('index page integration test', function(){
 		})
 	})
 	
+	it('should return Welcome to the payment system', function(done) {
+		request.get(`${baseUrl}/`, function(err, res, body) {
+			expect(err).to.be.null;
+			expect(res.statusCode).to.equal(200);
+			expect(body).to.equal('Welcome to the payment system');
+			done();
+		})
+	})
+	
 	it('should return 404', function(done) {
 		request.get(`${baseUrl}/cart/hello`, function(err, res, body) {
 			expect(err).to.be.null;
