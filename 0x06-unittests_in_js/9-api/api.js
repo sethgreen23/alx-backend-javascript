1f93a1776b6f254ad0324d9daff1fe1846207523
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-26 01:15:55 +0100

    task 9

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const port = 7865;
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+	const id = req.params.id;
+	res.send(`Payment methods for cart ${req.params.id}`);
+})
+
+app.listen(port, () => {
+	console.log('API available on localhost port 7865');
+})
+
+module.exports = app;
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+describe('index page integration test', function(){
+	const baseUrl = 'http://localhost:7865';
+	it('Payment methods for cart 12', function(done) {
+		request.get(`${baseUrl}/cart/12`, function(err, res, body) {
+			expect(err).to.be.null;
+			expect(res.statusCode).to.equal(200);
+			expect(body).to.equal('Payment methods for cart 12');
+			done();
+		})
+	})
+	
+	it('should return 404', function(done) {
+		request.get(`${baseUrl}/cart/hello`, function(err, res, body) {
+			expect(err).to.be.null;
+			expect(res.statusCode).to.equal(404);
+			done();
+		})
+	})
+})
